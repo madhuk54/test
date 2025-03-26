9c58f188d83603c68bde20bf463a5cfa7071a3ef
madhuk54 <krishnashetty98765@gmail.com>
2025-03-26 13:05:30 +0530

    Update Stu.java

--- Stu.java
+++ Stu.java
@@ -1,6 +1,6 @@
 public class Stu {
     public static void main(String[] args) {
-        String name = "John Doe";
+        String name = "John ";
         int choice = 1;
         
         String course = (choice == 1) ? "Java" : (choice == 2) ? "Python" : (choice == 3) ? "Data Structures" : "Invalid";
